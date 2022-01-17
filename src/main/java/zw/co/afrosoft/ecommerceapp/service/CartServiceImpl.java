3601da065643de404117ca470cd14118c06e516e
tyfahpundo <tafadzwaallentyfah@gmail.com>
2022-01-17 15:50:48 +0200

    Finished up add to cart API

--- src/main/java/zw/co/afrosoft/ecommerceapp/service/CartServiceImpl.java
+++ src/main/java/zw/co/afrosoft/ecommerceapp/service/CartServiceImpl.java
@@ -2,21 +2,33 @@ package zw.co.afrosoft.ecommerceapp.service;
 
 import org.springframework.stereotype.Service;
 import zw.co.afrosoft.ecommerceapp.dto.AddToCartDto;
+import zw.co.afrosoft.ecommerceapp.model.Cart;
 import zw.co.afrosoft.ecommerceapp.model.Product;
 import zw.co.afrosoft.ecommerceapp.model.User;
+import zw.co.afrosoft.ecommerceapp.repository.CartRepository;
+
+import java.util.Date;
 
 @Service
 public class CartServiceImpl implements CartService{
     private final ProductService productService;
+    private final CartRepository cartRepository;
 
-    public CartServiceImpl(ProductService productService) {
+    public CartServiceImpl(ProductService productService, CartRepository cartRepository) {
         this.productService = productService;
+        this.cartRepository = cartRepository;
     }
 
     @Override
     public void addToCart(AddToCartDto addToCartDto, User user) {
         //validate if product id is valid
         Product product = productService.findById(addToCartDto.getProductId());
-        //Save the CArt
+        //Save the cart
+        Cart cart = new Cart();
+        cart.setProduct(product);
+        cart.setUser(user);
+        cart.setQuantity(addToCartDto.getQuantity());
+        cart.setCreatedDate(new Date());
+        cartRepository.save(cart);
     }
 }
